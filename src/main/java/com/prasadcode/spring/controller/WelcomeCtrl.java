2cefbb31be0c8ae441e3a1e9f8b7f9a4c4c2de38
pradeo123 <prasad.s.deokar@gmail.com>
2022-04-14 15:55:09 +0530

    noncomliant code

--- src/main/java/com/prasadcode/spring/controller/WelcomeCtrl.java
+++ src/main/java/com/prasadcode/spring/controller/WelcomeCtrl.java
@@ -22,6 +22,14 @@ public class WelcomeCtrl {
 		modelview.setViewName("welcome");
 		return modelview;
 	}
+	
+	  int foo(int a) {
+		  int b = 12;
+		  if (a == 1) {
+		    return b;
+		  }
+		  return b;  // Noncompliant
+		}
 	//TODO
 	//FIX ME
 }
