1a9178c1f3fd0b268b864ee7afbdf504f647cf8f
prasad.s.deokar@gmail.com <prasad.s.deokar@gmail.com>
2022-04-19 19:29:27 +0530

    test

--- src/main/java/com/prasadcode/demo/Foo.java
+++ src/main/java/com/prasadcode/demo/Foo.java
@@ -6,7 +6,7 @@ public class Foo {
 	      return -1;
 	    }
 }
-
+ 
 	static class FooBar extends Bar {
 	    public int compareTo(FooBar rhs) {  // Noncompliant: Parameter should be of type Bar
 	      return 0;
