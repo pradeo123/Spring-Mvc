e8fc74c45e363361880671cbf163a79ee38edf98
pradeo123 <prasad.s.deokar@gmail.com>
2022-04-19 12:15:27 +0530

    bug added

--- src/main/java/com/prasadcode/spring/controller/WelcomeCtrl.java
+++ src/main/java/com/prasadcode/spring/controller/WelcomeCtrl.java
@@ -83,5 +83,14 @@ public void test_divide() {
 		  //TODO wirte code
 	  }
 	  z = 1 / z; // Noncompliant, possible division by zero
+	  
+	  int target = -5;
+	  int num = 3;
+
+	  target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+	  target =+ num; // Noncompliant; target = 3
+	  
 	}
+
+
 }
